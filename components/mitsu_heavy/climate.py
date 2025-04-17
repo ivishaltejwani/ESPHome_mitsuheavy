ef50a4b7a9259d2e27e192c03cb807f628924216
Vishal <57434902+ivishaltejwani@users.noreply.github.com>
2025-04-18 02:14:04 +0530

    Update climate.py

--- components/mitsu_heavy/climate.py
+++ components/mitsu_heavy/climate.py
@@ -1,29 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import CONF_ID, CONF_MODEL
+from esphome.const import CONF_ID
 
+CODEOWNERS = ["@mSapik"]
 AUTO_LOAD = ['climate_ir']
 
-mitsubishi_heavy_ns = cg.esphome_ns.namespace('mitsubishi_heavy')
-MitsubishiHeavyClimate = mitsubishi_heavy_ns.class_('MitsubishiHeavyClimate', climate_ir.ClimateIR)
+mhi_ns = cg.esphome_ns.namespace('mhi_zj')
+MhiClimate = mhi_ns.class_('MhiClimate', climate_ir.ClimateIR)
 
-Model = mitsubishi_heavy_ns.enum('Model')
-MODELS = {
-    'ZJ': Model.MODEL_ZJ,
-    'ZM': Model.MODEL_ZM,
-    'ZMP': Model.MODEL_ZMP,
-}
-
+#CONFIG_SCHEMA = climate_ir.CLIMATE_IR_SCHEMA.extend({
 CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(MitsubishiHeavyClimate),
-    cv.Required(CONF_MODEL): cv.enum(MODELS, upper=True)
+    cv.GenerateID(): cv.declare_id(MhiClimate),
 })
 
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield climate_ir.register_climate_ir(var, config)
-    cg.add(var.set_model(config[CONF_MODEL]))
-
-    cg.add_library('HeatpumpIR', None)
