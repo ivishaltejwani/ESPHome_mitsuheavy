758a784f479b62cbdd0e5feede615f0308176bc6
Vishal <57434902+ivishaltejwani@users.noreply.github.com>
2025-04-17 20:32:31 +0530

    Update mitsubishi_heavy.h

--- components/mitsu_heavy/mitsubishi_heavy.h
+++ components/mitsu_heavy/mitsubishi_heavy.h
@@ -1,34 +1,45 @@
 #pragma once
 
 #include "esphome/components/climate_ir/climate_ir.h"
+#include "esphome/components/remote_transmitter/remote_transmitter.h"
 
 namespace esphome {
 namespace mitsubishi_heavy {
 
-// Simple enum to represent models.
-enum Model {
-  MODEL_ZJ = 0,
-  MODEL_ZM = 1,
-  MODEL_ZMP = 2,
+enum MitsubishiModel {
+  MODEL_ZJ,
+  MODEL_ZM,
+  MODEL_ZMP
 };
-
-// Temperature
-const uint8_t MITSUBISHI_TEMP_MIN = 18;  // Celsius
-const uint8_t MITSUBISHI_TEMP_MAX = 30;  // Celsius
 
 class MitsubishiHeavyClimate : public climate_ir::ClimateIR {
  public:
   MitsubishiHeavyClimate()
       : climate_ir::ClimateIR(
-            MITSUBISHI_TEMP_MIN, MITSUBISHI_TEMP_MAX, 1.0f, true, true,
-            std::vector<climate::ClimateFanMode>{climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW,
-                                                 climate::CLIMATE_FAN_MEDIUM, climate::CLIMATE_FAN_HIGH}) {}
-  void set_model(Model model) { this->model_ = model; }
+            16.0f, 30.0f, 1.0f,  // Min, max, step
+            true, true,          // supports_dry, supports_fan_only
+            {
+              climate::CLIMATE_FAN_LOW,
+              climate::CLIMATE_FAN_MEDIUM,
+              climate::CLIMATE_FAN_HIGH,
+              climate::CLIMATE_FAN_AUTO
+            },
+            {
+              climate::CLIMATE_SWING_VERTICAL,
+              climate::CLIMATE_SWING_HORIZONTAL,
+              climate::CLIMATE_SWING_BOTH
+            },
+            {
+              climate::CLIMATE_PRESET_NONE,
+              climate::CLIMATE_PRESET_SLEEP,
+              climate::CLIMATE_PRESET_ECO
+            }) {}
+
+  void transmit_state() override;
+  void set_model(MitsubishiModel model) { this->model_ = model; }
 
  protected:
-  /// Transmit via IR the state of this climate controller.
-  void transmit_state() override;
-  Model model_;
+  MitsubishiModel model_;
 };
 
 }  // namespace mitsubishi_heavy
